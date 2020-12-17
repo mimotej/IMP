8fa87903f6be232b253546018f2dcd954982e94d
Michal Drla <47832967+mimotej@users.noreply.github.com>
2020-12-17 23:09:23 +0100

    Add files via upload

--- Sources/main.c
+++ Sources/main.c
@@ -29,351 +29,297 @@
  */
 
 #include "MK60D10.h"
+#include <time.h>
+#include <stdlib.h>
+/*
+ * Zákaldní makra pro práci s FitKitem
+ */
 #define BUTTON_UP_MASK 0x4000000
 #define BUTTON_LEFT_MASK 0x8000000
 #define BUTTON_DOWN_MASK 0x1000
 #define BUTTON_RIGHT_MASK 0x400
-#define PIEZZO_MASK 0x2000
-#define LED_R_MASK 0x20
-#define ROW_0 0x4000000
-#define ROW_1 0x1000000
-#define ROW_2 0x200
-#define ROW_3 0x2000000
-#define ROW_4 0x10000000
-#define ROW_5 0x80
-#define ROW_6 0x8000000
-#define ROW_7 0x20000000
+#define DIRECTION_RIGHT 0
+#define DIRECTION_LEFT 1
+#define DIRECTION_DOWN 2
+#define DIRECTION_UP 3
 #define CLEAR_ROW 0X0
-static int i = 0;
+#define SIZE_SNAKE 5
+int current_size=SIZE_SNAKE;
+int body_pos=0;
+int cycle_number=0;
 int x=2;
 int y=15;
-int direction_change=0;
+int direction=0;
+struct snake{
+	int x;
+	int y;
+};
+struct snake body_arr[SIZE_SNAKE];
+/*
+ * Základní inicalizace hada
+ */
+void Init_Snake(){
+	int tmp_x;
+	int tmp_y;
+	for(int i=0; i<SIZE_SNAKE; i++){
+		if(i == 0){
+			body_arr[0].x=2;
+			body_arr[0].y=15;
+		}else{
+			if(x-i <0){
+				int tmp= x-i;
+				tmp_x=8-tmp;
+			}else{
+				tmp_x=x-i;
+			}
+			body_arr[i].x=tmp_x;
+			body_arr[i].y=y;
+		}
+	}
+}
 #define GPIO_PIN_MASK	0x1Fu
 #define GPIO_PIN(x)		(((1)<<(x & GPIO_PIN_MASK)))
-void delay(uint64_t bound) {
-	for (uint64_t i=0; i < bound; i++) { __NOP(); }
-}
-void flash() {
-	GPIOB_PDOR ^= LED_R_MASK;
-    PTB->PDOR &= ~GPIO_PDOR_PDO(0x1);
-    delay(30000);
-    PTB->PDOR |= GPIO_PDOR_PDO(0x3C);
-    delay(30000);
-}
-void MCUInit(void)  {
+/*
+ * Inicializace MCU a vypnutí watchdogu
+ */
+void MCU_Init(void)  {
 	MCG->C4 |= ( MCG_C4_DMX32_MASK | MCG_C4_DRST_DRS(0x01) );
 	SIM->CLKDIV1 |= SIM_CLKDIV1_OUTDIV1(0x00);
 	WDOG_STCTRLH &= ~WDOG_STCTRLH_WDOGEN_MASK;
 }
-void PORTSinit(void){
-	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK | SIM_SCGC5_PORTE_MASK | SIM_SCGC5_PORTA_MASK;
-	PORTB->PCR[5] =  PORT_PCR_MUX(0x01);
-	PORTE->PCR[12] =   ( PORT_PCR_ISF(0x01)
-			| PORT_PCR_IRQC(0x0A)
-			| PORT_PCR_MUX(0x01)
-			| PORT_PCR_PE(0x01)
-			| PORT_PCR_PS(0x01));
-	PORTE->PCR[10] =  ( PORT_PCR_ISF(0x01)| PORT_PCR_IRQC(0x0A) | PORT_PCR_MUX(0x01)| PORT_PCR_PE(0x01)| PORT_PCR_PS(0x01));
-	PORTE->PCR[27] =  ( PORT_PCR_ISF(0x01)| PORT_PCR_IRQC(0x0A) | PORT_PCR_MUX(0x01)| PORT_PCR_PE(0x01)| PORT_PCR_PS(0x01));
-	PORTE->PCR[26] =  ( PORT_PCR_ISF(0x01)| PORT_PCR_IRQC(0x0A) | PORT_PCR_MUX(0x01)| PORT_PCR_PE(0x01)| PORT_PCR_PS(0x01));
+/*
+ * Inicializace všech nutných portù pro práci s FitKitem
+ */
+void PORTS_Init(void){
+	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK | SIM_SCGC5_PORTA_MASK;
+	SIM->SCGC6   |= SIM_SCGC6_PIT_MASK;
 
-	/* Set corresponding PTA pins (column activators of 74HC154) for GPIO functionality */
-	PORTA->PCR[8] = ( 0|PORT_PCR_MUX(0x01) );  // A0
-	PORTA->PCR[10] = ( 0|PORT_PCR_MUX(0x01) ); // A1
-	PORTA->PCR[6] = ( 0|PORT_PCR_MUX(0x01) );  // A2
-	PORTA->PCR[11] = ( 0|PORT_PCR_MUX(0x01) ); // A3
+	PORTE->PCR[10] =  ( PORT_PCR_ISF(0x01) | PORT_PCR_IRQC(0x0A) | PORT_PCR_MUX(0x01) | PORT_PCR_PE(0x01) | PORT_PCR_PS(0x01));
+	PORTE->PCR[12] =  ( PORT_PCR_ISF(0x01) | PORT_PCR_IRQC(0x0A) | PORT_PCR_MUX(0x01) | PORT_PCR_PE(0x01) | PORT_PCR_PS(0x01));
+	PORTE->PCR[27] =  ( PORT_PCR_ISF(0x01) | PORT_PCR_IRQC(0x0A) | PORT_PCR_MUX(0x01) | PORT_PCR_PE(0x01) | PORT_PCR_PS(0x01));
+	PORTE->PCR[26] =  ( PORT_PCR_ISF(0x01) | PORT_PCR_IRQC(0x0A) | PORT_PCR_MUX(0x01) | PORT_PCR_PE(0x01) | PORT_PCR_PS(0x01));
 
-	/* Set corresponding PTA pins (rows selectors of 74HC154) for GPIO functionality */
-	PORTA->PCR[26] = ( 0|PORT_PCR_MUX(0x01) );  // R0
-	PORTA->PCR[24] = ( 0|PORT_PCR_MUX(0x01) );  // R1
-	PORTA->PCR[9] = ( 0|PORT_PCR_MUX(0x01) );   // R2
-	PORTA->PCR[25] = ( 0|PORT_PCR_MUX(0x01) );  // R3
-	PORTA->PCR[28] = ( 0|PORT_PCR_MUX(0x01) );  // R4
-	PORTA->PCR[7] = ( 0|PORT_PCR_MUX(0x01) );   // R5
-	PORTA->PCR[27] = ( 0|PORT_PCR_MUX(0x01) );  // R6
-	PORTA->PCR[29] = ( 0|PORT_PCR_MUX(0x01) );  // R7
+	PORTA->PCR[8] = ( 0|PORT_PCR_MUX(0x01) );
+	PORTA->PCR[10] = ( 0|PORT_PCR_MUX(0x01) );
+	PORTA->PCR[6] = ( 0|PORT_PCR_MUX(0x01) );
+	PORTA->PCR[11] = ( 0|PORT_PCR_MUX(0x01) );
 
-	/* Set corresponding PTE pins (output enable of 74HC154) for GPIO functionality */
-	PORTE->PCR[28] = ( 0|PORT_PCR_MUX(0x01) ); // #EN
+	PORTA->PCR[26] = ( 0|PORT_PCR_MUX(0x01) );
+	PORTA->PCR[24] = ( 0|PORT_PCR_MUX(0x01) );
+	PORTA->PCR[9] = ( 0|PORT_PCR_MUX(0x01) );
+	PORTA->PCR[25] = ( 0|PORT_PCR_MUX(0x01) );
+	PORTA->PCR[28] = ( 0|PORT_PCR_MUX(0x01) );
+	PORTA->PCR[7] = ( 0|PORT_PCR_MUX(0x01) );
+	PORTA->PCR[27] = ( 0|PORT_PCR_MUX(0x01) );
+	PORTA->PCR[29] = ( 0|PORT_PCR_MUX(0x01) );
 
-    PTA->PDDR =  GPIO_PDDR_PDD(0x0010);
-    PTB->PDDR =  GPIO_PDDR_PDD(0x3C);
-	PTB->PDOR |= GPIO_PDOR_PDO(0x3C);
-	/* Change corresponding PTA port pins as outputs - might not work*/
+	PORTE->PCR[28] = ( 0|PORT_PCR_MUX(0x01) );
+
 	PTA->PDDR = GPIO_PDDR_PDD(0x3F000FC0);
-	/* Change corresponding PTE port pins as outputs */
 	PTE->PDDR = GPIO_PDDR_PDD( GPIO_PIN(28) );
+	NVIC_ClearPendingIRQ(PORTE_IRQn);
+	NVIC_EnableIRQ(PORTE_IRQn);
+
+
 }
-/* Conversion of requested column number into the 4-to-16 decoder control.  */
+/*
+ * Funkce pro výbìr sloupcù, pøevzato z ukázky poskytnuté Ing. Václavem Šimkem a Ph.D. Vojtìchem Mrázkem
+ */
 void column_select(unsigned int col_num)
 {
 	unsigned i, result, col_sel[4];
 
 	for (i =0; i<4; i++) {
-		result = col_num / 2;	  // Whole-number division of the input number
+		result = col_num / 2;
 		col_sel[i] = col_num % 2;
 		col_num = result;
 
 		switch(i) {
-
-			// Selection signal A0
 		    case 0:
 				((col_sel[i]) == 0) ? (PTA->PDOR &= ~GPIO_PDOR_PDO( GPIO_PIN(8))) : (PTA->PDOR |= GPIO_PDOR_PDO( GPIO_PIN(8)));
 				break;
 
-			// Selection signal A1
 			case 1:
 				((col_sel[i]) == 0) ? (PTA->PDOR &= ~GPIO_PDOR_PDO( GPIO_PIN(10))) : (PTA->PDOR |= GPIO_PDOR_PDO( GPIO_PIN(10)));
 				break;
 
-			// Selection signal A2
+
 			case 2:
 				((col_sel[i]) == 0) ? (PTA->PDOR &= ~GPIO_PDOR_PDO( GPIO_PIN(6))) : (PTA->PDOR |= GPIO_PDOR_PDO( GPIO_PIN(6)));
 				break;
 
-			// Selection signal A3
 			case 3:
 				((col_sel[i]) == 0) ? (PTA->PDOR &= ~GPIO_PDOR_PDO( GPIO_PIN(11))) : (PTA->PDOR |= GPIO_PDOR_PDO( GPIO_PIN(11)));
 				break;
 
-			// Otherwise nothing to do...
 			default:
 				break;
 		}
 	}
 }
+/*
+ * Výbìr jednotlivých øádkù
+ */
 void change_cell(int value){
 	switch(value){
-			case 7:
-				PTA->PDOR |= GPIO_PDOR_PDO(ROW_0);
-				break;
-			case 6:
-				PTA->PDOR |= GPIO_PDOR_PDO(ROW_1);
-				break;
-			case 5:
-				PTA->PDOR |= GPIO_PDOR_PDO(ROW_2);
-				break;
-			case 4:
-				PTA->PDOR |= GPIO_PDOR_PDO(ROW_3);
-				break;
-			case 3:
-				PTA->PDOR |= GPIO_PDOR_PDO(ROW_4);
-				break;
-			case 2:
-				PTA->PDOR |= GPIO_PDOR_PDO(ROW_5);
+			case 0:
+	            PTA->PDOR |= GPIO_PDOR_PDO( GPIO_PIN(29));
 				break;
 			case 1:
-				PTA->PDOR |= GPIO_PDOR_PDO(ROW_6);
+	            PTA->PDOR |= GPIO_PDOR_PDO( GPIO_PIN(27));
 				break;
-			case 0:
-				PTA->PDOR |= GPIO_PDOR_PDO(ROW_7);
+			case 2:
+	            PTA->PDOR |= GPIO_PDOR_PDO( GPIO_PIN(7));
+				break;
+			case 3:
+	            PTA->PDOR |= GPIO_PDOR_PDO( GPIO_PIN(28));
+				break;
+			case 4:
+	            PTA->PDOR |= GPIO_PDOR_PDO( GPIO_PIN(25));
+				break;
+			case 5:
+	            PTA->PDOR |= GPIO_PDOR_PDO( GPIO_PIN(9));
+				break;
+			case 6:
+	            PTA->PDOR |= GPIO_PDOR_PDO( GPIO_PIN(24));
+				break;
+			case 7:
+	            PTA->PDOR |= GPIO_PDOR_PDO( GPIO_PIN(26));
 				break;
 	}
 }
+/*
+ * Funkce pro pohyb hada, jednotlivé èlánky hada se pøesouvají na svého souseda pøièemž hlava se posune daným smìrem.
+ */
 void move_snake(int direction){
-		PTA->PDOR &= GPIO_PDOR_PDO(CLEAR_ROW);
 		switch(direction){
-				case 0:
-					if(x==0){
-						x+=1;
-						column_select(y);
-						change_cell(1);
-						change_cell(0);
-						change_cell(7);
-						break;
-					}
-					if(x == 7){
-						x=0;
-						column_select(y);
-						change_cell(0);
-						change_cell(7);
-						change_cell(6);
-					}
-					else{
-						column_select(y);
-						change_cell(x+1);
-						change_cell(x);
-						change_cell(x-1);
-						x+=1;
+				case DIRECTION_RIGHT:
+					for(int i=SIZE_SNAKE-1; i>=0; i--){
+						if(i == 0){
+							body_arr[i].x+=1;
+							if(body_arr[i].x == 8){
+								body_arr[i].x=0;
+							}
+						}else{
+							body_arr[i].x=body_arr[i-1].x;
+							body_arr[i].y=body_arr[i-1].y;
+						}
 					}
 					break;
-				case 1:
-					if(x==0){
-						x=7;
-						column_select(y);
-						change_cell(1);
-						change_cell(0);
-						change_cell(7);
-						break;
-					}
-					if(x == 7){
-						x-=1;
-						column_select(y);
-						change_cell(0);
-						change_cell(7);
-						change_cell(6);
-					}
-					else{
-						column_select(y);
-						change_cell(x+1);
-						change_cell(x);
-						change_cell(x-1);
-						x-=1;
+				case DIRECTION_LEFT:
+					for(int i=SIZE_SNAKE-1; i>=0; i--){
+						if(i == 0){
+							body_arr[i].x-=1;
+							if(body_arr[i].x == -1){
+								body_arr[i].x=7;
+							}
+						}else{
+							body_arr[i].x=body_arr[i-1].x;
+							body_arr[i].y=body_arr[i-1].y;
+						}
 					}
 					break;
-				case 2:
-						if (direction_change==-1){
-							for(int a=0; a < 20; a++){
-									change_cell(x);
-									column_select(y+1);
-									delay(5000);
-									change_cell(x);
-									column_select(y);
-									delay(5000);
-									change_cell(x-1);
-									column_select(y);
-									delay(5000);
-									direction_change=0;
-								}
-						}
-					if(y==15){
-						y=0;
-						for(int a=0; a < 20; a++){
-							change_cell(x);
-							column_select(15);
-							delay(5000);
-							column_select(14);
-							delay(5000);
-							column_select(0);
-							delay(5000);
-						}
-						break;
-					}
-					if(y == 0){
-						y+=1;
-						for(int a=0; a < 20; a++){
-						change_cell(x);
-						column_select(0);
-						delay(5000);
-						column_select(15);
-						delay(5000);
-						column_select(1);
-						delay(5000);
+				case DIRECTION_DOWN:
+					for(int i=SIZE_SNAKE-1; i>=0; i--){
+						if(i == 0){
+							body_arr[i].y+=1;
+							if(body_arr[i].y == 16){
+								body_arr[i].y=0;
+							}
+						}else{
+							body_arr[i].x=body_arr[i-1].x;
+							body_arr[i].y=body_arr[i-1].y;
 						}
 					}
-					else{
-						for(int a=0; a < 20; a++){
-						change_cell(x);
-						column_select(y+1);
-						delay(5000);
-						column_select(y);
-						delay(5000);
-						column_select(y-1);
-						delay(5000);
-						}
-						y+=1;
-					}
-
 					break;
-				case 3:
-					if(y==0){
-						y=15;
-						for(int a=0; a < 20; a++){
-							change_cell(x);
-							column_select(15);
-							delay(5000);
-							column_select(0);
-							delay(5000);
-							column_select(1);
-							delay(5000);
+				case DIRECTION_UP:
+					for(int i=SIZE_SNAKE-1; i>=0; i--){
+						if(i == 0){
+							body_arr[i].y-=1;
+							if(body_arr[i].y == -1){
+								body_arr[i].y=15;
+							}
+						}else{
+							body_arr[i].x=body_arr[i-1].x;
+							body_arr[i].y=body_arr[i-1].y;
 						}
-						break;
-					}
-					if(y == 15){
-						y-=1;
-						for(int a=0; a < 20; a++){
-						change_cell(x);
-						column_select(14);
-						delay(5000);
-						column_select(15);
-						delay(5000);
-						column_select(0);
-						delay(5000);
-						}
-					}
-					else{
-						for(int a=0; a < 20; a++){
-						change_cell(x);
-						column_select(y+1);
-						delay(5000);
-						column_select(y);
-						delay(5000);
-						column_select(y-1);
-						delay(5000);
-						}
-						y-=1;
 					}
 					break;
 				default:
 					break;
 		}
 }
-
-
+/*
+ * Pøerušení èasovaèem
+ */
+void PIT0_IRQHandler(void){
+	PIT_TCTRL0 = 0;
+	PIT_TFLG0 |= PIT_TFLG_TIF_MASK;
+	PTA->PDOR &= GPIO_PDOR_PDO(CLEAR_ROW);
+	column_select(body_arr[body_pos].y);
+	change_cell(body_arr[body_pos].x);
+	if(cycle_number == 24){
+		move_snake(direction);
+		cycle_number=0;
+		body_pos=0;
+	}else{
+		body_pos+=1;
+	}
+	if(body_pos == SIZE_SNAKE){
+		body_pos=0;
+		cycle_number+=1;
+	}
+	PIT_TCTRL0 |= PIT_TCTRL_TEN_MASK | PIT_TCTRL_TIE_MASK;
+}
+/*
+ * Pøerušení pøi stisku tlaèítek
+ *
+ */
+void PORTE_IRQHandler(void){
+	 if (PORTE->ISFR & BUTTON_DOWN_MASK){
+		 	 if(direction != DIRECTION_UP){
+		 		 direction=DIRECTION_DOWN;
+		 	 }
+		 }
+		 if (PORTE->ISFR & BUTTON_LEFT_MASK){
+			 if(direction != DIRECTION_RIGHT){
+				 direction=DIRECTION_LEFT;
+			 }
+		 }
+		 if (PORTE->ISFR & BUTTON_UP_MASK){
+			 if(direction != DIRECTION_DOWN){
+				 direction=DIRECTION_UP;
+			 }
+		 }
+		 if (PORTE->ISFR & BUTTON_RIGHT_MASK){
+			 if(direction != DIRECTION_LEFT){
+				 direction=DIRECTION_RIGHT;
+			 }
+		 }
+		PORTE->ISFR = BUTTON_UP_MASK | BUTTON_LEFT_MASK | BUTTON_DOWN_MASK | BUTTON_RIGHT_MASK;
+}
+/*
+ * Inicializace èasovaèe PIT
+ */
+void Init_PIT(void){
+	NVIC_ClearPendingIRQ(PIT0_IRQn);
+	NVIC_EnableIRQ(PIT0_IRQn);
+	PIT_MCR = 0x00;
+	PIT_TCTRL0 = 0x00;
+	PIT_LDVAL0 = 49990;
+	PIT_TCTRL0 = PIT_TCTRL_TIE_MASK;
+	PIT_TFLG1 = 0x01;
+	PIT_TCTRL0 |= PIT_TCTRL_TEN_MASK;
+}
 int main(void)
 {
 
-    /* Write your code here */
-
-
-	MCUInit();
-	PORTSinit();
-	int a=0;
-	int direction=0;
-	while(1){
-		if(direction == 2 || direction ==3){
-			move_snake(direction);
-		}
-		if(direction == 0 || direction == 1){
-				move_snake(direction);
-				delay(300000);
-		}
-	 if (PORTE->ISFR & BUTTON_DOWN_MASK){
-		 	 if(direction == 0){
-		 		 direction_change=1;
-		 	 }else if(direction == 1){
-		 		direction_change = -1;
-		 	 }
-		 	 direction=2;
-		 }
-		 if (PORTE->ISFR & BUTTON_LEFT_MASK){
-		 	 if(direction == 2 || direction ==3){
-		 		 direction_change=1;
-		 	 }
-		 	 direction=1;
-		 }
-		 if (PORTE->ISFR & BUTTON_UP_MASK){
-		 	 if(direction == 0){
-		 		 direction_change=1;
-		 	 }else if(direction == 1){
-		 		direction == -1;
-		 	 }
-		 	 direction=3;
-		 }
-		 if (PORTE->ISFR & BUTTON_RIGHT_MASK){
-		 	 if(direction == 2 || direction == 3){
-		 		 direction_change=1;
-		 	 }
-		 	 direction=0;
-		 }
-		PORTE->ISFR = BUTTON_UP_MASK | BUTTON_LEFT_MASK | BUTTON_DOWN_MASK | BUTTON_RIGHT_MASK;
-
-	}
-    /* This for loop should be replaced. By default this loop allows a single stepping. */
+	MCU_Init();
+	PORTS_Init();
+	Init_Snake();
+	Init_PIT();
     for (;;) {
-        i++;
     }
-    /* Never leave main */
     return 0;
 }
 ////////////////////////////////////////////////////////////////////////////////
